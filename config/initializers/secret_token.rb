93784a1b57a769f19d248726ad916929ada78a1e
michael verdi <michael.v.verdi@gmail.com>
2014-05-24 13:50:41 -0400

    initializing new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GifChat::Application.config.secret_key_base = '5dba222e30626e42e3f784df477b2643a3cd9868f0ccfb551d7a989ebeb6557a88b59a5c65521999a7265a85a061148c5b05160bc1a330dc6127a758ce3a96f4'
