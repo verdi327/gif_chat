24a6f89c0f5498564119160ceb97edf23687d0b4
michael verdi <michael.v.verdi@gmail.com>
2014-05-25 18:21:02 -0400

    added apikey model, locked down all requests to require an apikey passed in the headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :verified_request?
+  before_action :restrict_access
 
   def verified_request?
     if request.content_type == "application/json"
       true
     else
       super()
+    end
+  end
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525200719) do
+ActiveRecord::Schema.define(version: 20140525221044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: true do |t|
+    t.string   "access_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "conversations", force: true do |t|
     t.datetime "created_at"
